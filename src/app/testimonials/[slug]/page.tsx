ac6d81259d47fdfd0257c378952d31fcfbce957d
Soumya Biswas <biswassoumya17@gmail.com>
2025-08-22 21:20:20 +0530

    update testimonial card design

--- src/app/testimonials/[slug]/page.tsx
+++ src/app/testimonials/[slug]/page.tsx
@@ -1,39 +1,45 @@
 import { notFound } from "next/navigation";
 import testimonials from "@/data/testimonials.json";
+import SectionHeader from "@/components/common/SectionHeaderProps";
+import Image from "next/image";
 
 type Props = {
-  params: { slug: string };
+    params: { slug: string };
 };
 
 export async function generateStaticParams() {
-  return testimonials.map((t) => ({
-    slug: t.slug,
-  }));
+    return testimonials.map((t) => ({
+        slug: t.slug,
+    }));
 }
 
 export async function generateMetadata({ params }: Props) {
-  const testimonial = testimonials.find((t) => t.slug === params.slug);
-  if (!testimonial) return { title: "Testimonial Not Found" };
-  return {
-    title: testimonial.title,
-    description: testimonial.description,
-  };
+    const testimonial = testimonials.find((t) => t.slug === params.slug);
+    if (!testimonial) return { title: "Testimonial Not Found" };
+    return {
+        title: testimonial.title,
+        description: testimonial.description,
+    };
 }
 
 export default function TestimonialDetails({ params }: Props) {
-  const testimonial = testimonials.find((t) => t.slug === params.slug);
+    const testimonial = testimonials.find((t) => t.slug === params.slug);
 
-  if (!testimonial) return notFound();
+    if (!testimonial) return notFound();
 
-  return (
-    <main className="testimonial-details-container">
-      <div className="testimonial-details-image">
-        <img src={testimonial.image} alt={testimonial.title} />
-      </div>
-      <div className="testimonial-details-content">
-        <h1>{testimonial.title}</h1>
-        <p>{testimonial.details}</p>
-      </div>
-    </main>
-  );
+    return (
+        <section>
+            <SectionHeader
+                title={testimonial.title} />
+            <main className="testimonial-details-container">
+                <div className="testimonial-details-image">
+                    <Image src={testimonial.image} alt={testimonial.title} width={500} height={500} layout="100" />
+                </div>
+                <div className="testimonial-details-content">
+                    <h1>{testimonial.title}</h1>
+                    <p>{testimonial.details}</p>
+                </div>
+            </main>
+        </section>
+    );
 }
--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,26 +1,28 @@
 import TestimonialCard from '@/components/card/TestimonialsCard'
 import React from 'react'
 import testimonials from "../../data/testimonials.json"
+import SectionHeader from '@/components/common/SectionHeaderProps'
 const TestiMonialsPage = () => {
-  return (
-<section>
+    return (
+        <section>
 
-  <main className="testimonials-page-container">
-      <h1 className="testimonials-page-title">Testimonials</h1>
-      <div className="testimonials-grid">
-        {testimonials.map((testimonial) => (
-          <TestimonialCard
-            key={testimonial.id}
-            slug={testimonial.slug}
-            title={testimonial.title}
-            description={testimonial.description}
-            image={testimonial.image}
-          />
-        ))}
-      </div>
-    </main>
-    </section>
-  )
+            <SectionHeader
+                title='Our Testimonials & Success Stories' />
+            <main className="testimonials-page-container">
+                <div className="testimonials-grid">
+                    {testimonials.map((testimonial) => (
+                        <TestimonialCard
+                            key={testimonial.id}
+                            slug={testimonial.slug}
+                            title={testimonial.title}
+                            description={testimonial.description}
+                            image={testimonial.image}
+                        />
+                    ))}
+                </div>
+            </main>
+        </section>
+    )
 }
 
 export default TestiMonialsPage
