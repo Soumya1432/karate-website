6b62903a4e7f2de77faefc6a1bb7732e0dc343a8
Soumya Biswas <biswassoumya17@gmail.com>
2025-08-22 16:41:30 +0530

    completed galllery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,17 +1,47 @@
-import SectionHeader from '@/components/common/SectionHeaderProps'
-import React from 'react'
+// import SectionHeader from '@/components/common/SectionHeaderProps'
+// import React from 'react'
 
-const OurGalleryPage = () => {
-    return (
-        <section>
-            <SectionHeader
-                title='Our Gallery' />
-            <div className=''>
+// const OurGalleryPage = () => {
+//     return (
+//         <section>
+//             <SectionHeader
+//                 title='Our Gallery' />
+//             <div className=''>
 
-            </div>
+//             </div>
 
-        </section>
-    )
-}
+//         </section>
+//     )
+// }
 
-export default OurGalleryPage
+// export default OurGalleryPage
+
+
+"use client";
+
+import React from "react";
+import images from "../../data/images.json"
+import Image from "next/image";
+import SectionHeader from "@/components/common/SectionHeaderProps";
+
+const Gallery: React.FC = () => {
+  return (
+    <section>
+        <SectionHeader
+         title="Our Gallery" 
+         />
+    <div className="gallery-container">
+      <h2 className="gallery-title">Image Gallery</h2>
+      <div className="gallery-grid">
+        {images.map((img) => (
+          <div className="gallery-item" key={img.id}>
+            <Image src={img.src} alt={img.alt} width={500} height={500} layout="100"/>
+          </div>
+        ))}
+      </div>
+    </div>
+    </section>
+  );
+};
+
+export default Gallery;
